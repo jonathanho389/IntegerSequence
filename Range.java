b1e08421c0bd4cab9da949ba84a316627244b3fa
Jonathan <>
2020-12-07 16:52:06 -0500

    created Range constructor and reset

---
+++ Range.java
@@ -0,0 +1,12 @@
+public class Range{
+  private int start, end, current;
+  public Range(int start, int end){
+    this.start = start;
+    this.end = end;
+    this.start = current;
+  }
+
+  public void reset(){
+    current = start;
+  }
+}
