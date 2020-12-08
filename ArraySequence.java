bf42a74c86bf1ec8308a74e79f9b53778d90231f
Jonathan <>
2020-12-08 12:38:28 -0500

    coded reset

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,10 +1,17 @@
+import java.util.NoSuchElementException;
 public class ArraySequence{
   private int currentIndex;
   private int[] data;
 
   public ArraySequence(int[] other){
     data = other;
+    currentIndex = data[0];
   }
 
+  public void reset(){
+    currentIndex = data[0];
+  }
+
+  
 
 }
